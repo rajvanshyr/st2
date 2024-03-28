6fc3ad96b3cebe0f63de5bd6f0fc1962560bf792
rajvanshyr <rajvanshyr@utexas.edu>
2024-03-27 17:18:06 -0700

    L8

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if anthropic_api_key:
 		],
 		)
 	r=message.content
-with st.expander('About this app'+r):
+with st.expander('About this app'+str(r)):
 	st.markdown('**What can this app do?**')
 	st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
 	st.markdown('**How to use the app?**')
@@ -92,26 +92,3 @@ components.html(
 </script>
 		""", height=600,
 		)
-
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
-
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
-# Display DataFrame
-
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-														column_config={"year": st.column_config.TextColumn("Year")},
-														num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
-
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-						x=alt.X('year:N', title='Year'),
-						y=alt.Y('gross:Q', title='Gross earnings ($)'),
-						color='genre:N'
-						).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
